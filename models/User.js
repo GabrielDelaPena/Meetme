2f71775b501f708fc27b4cc74fa01ba66ecd7aae
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-01-14 21:28:25 +0100

    Update User table.

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    role: {
+      type: String,
+      required: true,
+    },
     firstname: {
       type: String,
       required: true,
